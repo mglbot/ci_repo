889fd40b76ebedd7e5f5161def8cdb82719ae292
Graham Lopez <lopezmg@ornl.gov>
2018-03-27 23:49:19 -0400

    improved travis script

--- testing/travis.sh
+++ testing/travis.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo ""
 echo ""
 echo "starting new test for real full precision"
 echo ""
 echo ""
+
+STATUS=0
 
 cd build
 
@@ -17,32 +19,39 @@ echo checking J1 full precision
 echo ----------------------------------------------------
 echo
 
-./bin/check_wfc -f J1
+./bin/check_wfc -f J1 | awk 'BEGIN{status=1} /All checks passed for J1/{status=0} 1; END{exit(status)}'
+if [ $? != 0 ] ; then STATUS=1 ; fi
 
 echo
 echo checking J2 full precision
 echo ----------------------------------------------------
 echo
 
-./bin/check_wfc -f J2
+./bin/check_wfc -f J2 | awk 'BEGIN{status=1} /All checks passed for J2/{status=0} 1; END{exit(status)}'
+if [ $? != 0 ] ; then STATUS=1 ; fi
 
 echo
 echo checking J3 full precision
 echo ----------------------------------------------------
 echo
 
-./bin/check_wfc -f J3
+./bin/check_wfc -f J3 | awk 'BEGIN{status=1} /All checks passed for J3/{status=0} 1; END{exit(status)}'
+if [ $? != 0 ] ; then STATUS=1 ; fi
 
 echo
 echo checking Spline SPO full precision
 echo ----------------------------------------------------
 echo
 
-./bin/check_spo
+./bin/check_spo | awk 'BEGIN{status=1} /All checks passed for spo/{status=0} 1; END{exit(status)}'
+if [ $? != 0 ] ; then STATUS=1 ; fi
 
 echo
 echo checking Determinant update full precision
 echo ----------------------------------------------------
 echo
 
-./bin/check_determinant
+./bin/check_determinant | awk 'BEGIN{status=1} /All checks passed for determinant/{status=0} 1; END{exit(status)}'
+if [ $?  != 0] ; then STATUS=1 ; fi
+
+exit ${STATUS}
